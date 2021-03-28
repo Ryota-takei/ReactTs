2d3b2634a6aee69b99f8b7a982216cb535df5180
Ryota-takei <wlol0747@yahoo.co.jp>
2021-03-28 10:08:50 +0900

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Counter } from "./Counter";
 
 interface AppProps {
   message?: string;
 }
 
-const App: React.FunctionComponent<AppProps> = ({message}) => {
-
+const App: React.FunctionComponent<AppProps> = ({ message }) => {
   return (
     <div className="App">
-      {message}
+      <Counter />
     </div>
   );
-  
-}
+};
 
 App.defaultProps = {
-  message: "Hello, defaultProps!"
-}
+  message: "Hello, defaultProps!",
+};
 export default App;
